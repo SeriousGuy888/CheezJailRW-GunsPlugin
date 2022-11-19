a2a66e350c668f1e6ca17ac138c30d2ed07b7048
SeriousGuy888 <38299346+SeriousGuy888@users.noreply.github.com>
2022-11-19 01:36:26 -0500

    refactored gunevents, putting more stuff in separate functions

--- src/main/java/io/github/seriousguy888/cheezjailrwguns/events/GunEvents.java
+++ src/main/java/io/github/seriousguy888/cheezjailrwguns/events/GunEvents.java
@@ -41,33 +41,19 @@ public class GunEvents implements Listener {
     ItemStack heldItem = player.getInventory().getItemInMainHand();
 
     AbstractCustomItem heldCustomItem = CustomItemUtils.getCustomItem(heldItem);
-    if (heldCustomItem == null)
+    if (heldCustomItem == null) // If this is not a plugin managed custom item
       return;
-    if (!(heldCustomItem instanceof AbstractGun heldGunType))
+    if (!(heldCustomItem instanceof AbstractGun heldGunType)) // If this is not a gun
       return;
 
+    event.setCancelled(true);
     if (action.equals(Action.RIGHT_CLICK_AIR) || action.equals(Action.RIGHT_CLICK_BLOCK)) {
-      event.setCancelled(true);
-      // No shooting while reloading
       if (reloadingPlayers.contains(player))
         return;
-
-      long playerLastFired = lastFiredGun.getOrDefault(player, 0L);
-      long diff = System.currentTimeMillis() - playerLastFired;
-      if (diff < heldGunType.getCooldownMs())
+      if (!gunNotInCooldown(player, heldGunType))
         return;
-
-
-      int ammo = heldGunType.getAmmo(heldItem);
-      if (ammo <= 0) {
-        player.getWorld().playSound(player.getLocation(), Sound.BLOCK_NOTE_BLOCK_HAT, 0.5f, 1);
-        player.spigot().sendMessage(ChatMessageType.ACTION_BAR,
-            TextComponent.fromLegacyText(ChatColor.RED + "Weapon has no ammo!"));
+      if (!gunHasAmmo(player, heldItem, heldGunType))
         return;
-      }
-
-      heldGunType.setAmmo(heldItem, ammo - 1);
-      heldGunType.updateAmmoDisplay(heldItem);
 
       player.getWorld().playSound(
           player.getLocation(),
@@ -79,9 +65,9 @@ public class GunEvents implements Listener {
 
       lastFiredGun.put(player, System.currentTimeMillis());
 
+      float range = heldGunType.getRange();
       Location playerLoc = player.getEyeLocation();
       Vector playerDir = playerLoc.getDirection().normalize();
-      float range = heldGunType.getRange();
       RayTraceResult result = player.getWorld().rayTrace(
           playerLoc,
           playerDir,
@@ -92,91 +78,130 @@ public class GunEvents implements Listener {
           entity -> !entity.equals(player)
       );
 
+      drawBulletParticleLine(player, result, playerLoc, playerDir, range);
+      damageHitEntity(player, result, heldGunType);
 
-      // Use hit location if the raycast hit something, otherwise draw the line to the
-      // maximum range that it could have hit something.
-      Location particleLineEnd = (
-          result == null ?
-              playerDir.clone().multiply(range).add(playerLoc.toVector()) :
-              result.getHitPosition()
-      ).toLocation(player.getWorld());
-
-      Location particleLineStart = playerLoc.clone();
-      Vector particleLineDir = particleLineEnd.toVector().subtract(playerLoc.toVector()).normalize();
-      double particleLineStep = 0.5;
-      double distBetween = particleLineEnd.distance(playerLoc);
-
-      // Draw a line of particles to illustrate where the bullet travelled
-      for (int i = 1; i < distBetween / particleLineStep; i++) {
-        particleLineDir.multiply(i * particleLineStep);
-        particleLineStart.add(particleLineDir);
-        player.getWorld().spawnParticle(
-            Particle.CRIT, particleLineStart, 1, 0, 0, 0, 0);
-        particleLineStart.subtract(particleLineDir);
-        particleLineDir.normalize();
-      }
-
-      player.getWorld().spawnParticle(
-          Particle.SMOKE_NORMAL, playerLoc, 5, 0.2, 0.2, 0.2, 0.1);
-
-
-      if (result == null)
-        return;
-      if (!(result.getHitEntity() instanceof LivingEntity hitEntity))
-        return;
-
-      float dmg = heldGunType.getDamage();
-      hitEntity.damage(dmg, player);
-      hitEntity.setLastDamageCause(new EntityDamageEvent(player, EntityDamageEvent.DamageCause.PROJECTILE, dmg));
-      hitEntity.setNoDamageTicks(0);
-
-    } else if (event.getAction().equals(Action.LEFT_CLICK_AIR) ||
-        event.getAction().equals(Action.LEFT_CLICK_BLOCK)) {
-
-      // Don't run reloading code if gun is already at full ammo
-      if(heldGunType.getAmmo(heldItem) >= heldGunType.getMaxAmmo())
-        return;
-
-      reloadingPlayers.add(player);
-      player.getWorld().playSound(
-          player.getLocation(),
-          heldGunType.getSoundString(AbstractGun.GunSoundType.RELOAD),
-          1,
-          1
-      );
-
-      new BukkitRunnable() {
-        @Override
-        public void run() {
-          ItemStack currHeldItem = player.getInventory().getItemInMainHand();
-          if (!heldItem.equals(currHeldItem)) {
-            cancel();
-            reloadingPlayers.remove(player);
-            return;
-          }
-
-          int ammo = heldGunType.getAmmo(heldItem);
-          int maxAmmo = heldGunType.getMaxAmmo();
-          if (ammo >= maxAmmo) {
-            cancel();
-            reloadingPlayers.remove(player);
-            return;
-          }
-
-          ItemStack ammoStack = heldGunType.getCorrectAmmoStack(currHeldItem, player.getInventory());
-          if (ammoStack == null) {
-            cancel();
-            reloadingPlayers.remove(player);
-          } else {
-            ammoStack.setAmount(ammoStack.getAmount() - 1);
-            heldGunType.reloadGun(currHeldItem, 1);
-
-          }
-
-        }
-      }.runTaskTimer(plugin, 0, (heldGunType.getReloadTicks() / heldGunType.getMaxAmmo()));
+    } else if (action.equals(Action.LEFT_CLICK_AIR) || action.equals(Action.LEFT_CLICK_BLOCK)) {
+      tryReloadGun(player, heldItem, heldGunType);
     }
   }
+  
 
+  private boolean gunNotInCooldown(Player player,
+                                   AbstractGun heldGunType) {
+    long playerLastFired = lastFiredGun.getOrDefault(player, 0L);
+    long diff = System.currentTimeMillis() - playerLastFired;
+    return diff > heldGunType.getCooldownMs();
+  }
+
+  private boolean gunHasAmmo(Player player,
+                             ItemStack heldItem,
+                             AbstractGun heldGunType) {
+    int ammo = heldGunType.getAmmo(heldItem);
+    if (ammo <= 0) {
+      player.getWorld().playSound(player.getLocation(), Sound.BLOCK_NOTE_BLOCK_HAT, 0.5f, 1);
+      player.spigot().sendMessage(ChatMessageType.ACTION_BAR,
+          TextComponent.fromLegacyText(ChatColor.RED + "Weapon has no ammo!"));
+      return false;
+    }
+
+    heldGunType.setAmmo(heldItem, ammo - 1);
+    heldGunType.updateAmmoDisplay(heldItem);
+    return true;
+  }
+
+  private void damageHitEntity(Player player,
+                               RayTraceResult traceResult,
+                               AbstractGun heldGunType) {
+    if (traceResult == null)
+      return;
+    if (!(traceResult.getHitEntity() instanceof LivingEntity hitEntity))
+      return;
+
+    float dmg = heldGunType.getDamage();
+    hitEntity.damage(dmg, player);
+    hitEntity.setLastDamageCause(new EntityDamageEvent(player, EntityDamageEvent.DamageCause.PROJECTILE, dmg));
+    hitEntity.setNoDamageTicks(0);
+  }
+
+  private void drawBulletParticleLine(Player player,
+                                      RayTraceResult traceResult,
+                                      Location playerLoc,
+                                      Vector playerDir,
+                                      float range) {
+    // Use hit location if the raycast hit something, otherwise draw the line to the
+    // maximum range that it could have hit something.
+    Location particleLineEnd = (
+        traceResult == null ?
+            playerDir.clone().multiply(range).add(playerLoc.toVector()) :
+            traceResult.getHitPosition()
+    ).toLocation(player.getWorld());
+
+    Location particleLineStart = playerLoc.clone();
+    Vector particleLineDir = particleLineEnd.toVector().subtract(playerLoc.toVector()).normalize();
+    double particleLineStep = 0.5;
+    double distBetween = particleLineEnd.distance(playerLoc);
+
+    // Draw a line of particles to illustrate where the bullet travelled
+    for (int i = 1; i < distBetween / particleLineStep; i++) {
+      particleLineDir.multiply(i * particleLineStep);
+      particleLineStart.add(particleLineDir);
+      player.getWorld().spawnParticle(
+          Particle.CRIT, particleLineStart, 1, 0, 0, 0, 0);
+      particleLineStart.subtract(particleLineDir);
+      particleLineDir.normalize();
+    }
+
+    player.getWorld().spawnParticle(
+        Particle.SMOKE_NORMAL, playerLoc, 5, 0.2, 0.2, 0.2, 0.1);
+  }
+
+
+  private void tryReloadGun(Player player,
+                            ItemStack heldItem,
+                            AbstractGun heldGunType) {
+    // Don't run reloading code if gun is already at full ammo
+    if (heldGunType.getAmmo(heldItem) >= heldGunType.getMaxAmmo())
+      return;
+
+    reloadingPlayers.add(player);
+    player.getWorld().playSound(
+        player.getLocation(),
+        heldGunType.getSoundString(AbstractGun.GunSoundType.RELOAD),
+        1,
+        1
+    );
+
+    new BukkitRunnable() {
+      @Override
+      public void run() {
+        ItemStack currHeldItem = player.getInventory().getItemInMainHand();
+        if (!heldItem.equals(currHeldItem)) {
+          cancel();
+          reloadingPlayers.remove(player);
+          return;
+        }
+
+        int ammo = heldGunType.getAmmo(heldItem);
+        int maxAmmo = heldGunType.getMaxAmmo();
+        if (ammo >= maxAmmo) {
+          cancel();
+          reloadingPlayers.remove(player);
+          return;
+        }
+
+        ItemStack ammoStack = heldGunType.getCorrectAmmoStack(currHeldItem, player.getInventory());
+        if (ammoStack == null) {
+          cancel();
+          reloadingPlayers.remove(player);
+        } else {
+          ammoStack.setAmount(ammoStack.getAmount() - 1);
+          heldGunType.reloadGun(currHeldItem, 1);
+
+        }
+
+      }
+    }.runTaskTimer(plugin, 0, (heldGunType.getReloadTicks() / heldGunType.getMaxAmmo()));
+  }
 }
 
